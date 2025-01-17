3a15f1f20857560de3329effd9d7c2e44bc0b526
Giuseppe <g.lombardi11@studenti.unipi.it>
2025-01-17 17:42:47 +0100

    fix shape

--- homaILS/printing/results.py
+++ homaILS/printing/results.py
@@ -1,18 +1,22 @@
-def print_2D_localization(model_positions, measured_positions, estimated_positions):
+def print_2D_localization(model_positions, observed_positions, estimated_positions):
     """
     Print the 2D localization results.
     """
     # Check if the lengths of the lists are the same
-    if not (len(model_positions) == len(measured_positions) == len(estimated_positions)):
+    if not (len(model_positions) == len(observed_positions) == len(estimated_positions)):
         raise ValueError("Lengths of the input lists are not the same")
     
     # Print results
     for i in range(len(model_positions)):
         print(f"Step {i+1}:")
+
         if model_positions[i] is not None:
-            print(f"  Model Position: x={model_positions[i][0]:.2f}, y={model_positions[i][1]:.2f}")
-        if measured_positions[i] is not None:
-            print(f"  Measured Pos:   x={measured_positions[i][0]:.2f}, y={measured_positions[i][1]:.2f}")
+            p = model_positions[i].reshape(-1, 1)
+            print(f"  Model Position: x={p[0, 0]:.2f}, y={p[1, 0]:.2f}")
+        if observed_positions[i] is not None:
+            p = observed_positions[i].reshape(-1, 1)
+            print(f"  Observed Pos:  x={p[0, 0]:.2f}, y={p[1, 0]:.2f}")
         if estimated_positions[i] is not None:
-            print(f"  Estimated Pos:  x={estimated_positions[i][0]:.2f}, y={estimated_positions[i][1]:.2f}")
+            p = estimated_positions[i].reshape(-1, 1)
+            print(f"  Estimated Pos:  x={p[0, 0]:.2f}, y={p[1, 0]:.2f}")
         print("------------------------------------------------")
